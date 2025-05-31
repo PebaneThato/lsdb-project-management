7de2adaaedabe9a220db0fa605b3d7a40e7a971b
Thato Pebane <thato.pabane@bmwithub.co.za>
2025-06-01 01:31:55 +0200

    Add Details Card And User Interface Model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lsdb-registration-form';
+  submittedData: any = null;
 }
---
+++ src/app/app.interface.ts
@@ -0,0 +1,8 @@
+export interface User {
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+  password: string;
+  contactNumber: string;
+  userRole: string;
+}
