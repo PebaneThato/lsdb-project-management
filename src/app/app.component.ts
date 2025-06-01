22451f1ed42035eea420b5901841cc2c8d5876ae
Thato Pebane <thato.pabane@bmwithub.co.za>
2025-06-01 02:33:46 +0200

    Display Form Data Into Card Details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './app.interface';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lsdb-registration-form';
-  submittedData: any = null;
+  userForm: FormGroup;
+  submittedUserData: User | null = null;
 
   userRoles = [
     { value: 'Accountant', label: 'Accountant' },
     { value: 'Administrator', label: 'Administrator' },
     { value: 'Human Resource', label: 'Human Resource' },
   ];
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      emailAddress: ['', Validators.required],
+      password: ['', Validators.required],
+      contactNumber: ['', Validators.required],
+      userRole: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.userForm.valid) {
+      let maskedPassword = this.userForm.value.password.replace(/./g, '•');
+      this.userForm.patchValue({
+        password: maskedPassword
+      });
+      this.submittedUserData = this.userForm.value as User;
+    }
+  }
+
 }
