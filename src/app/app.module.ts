fff5f2e3186af00f6de7daf48e5995b72496374b
Thato Pebane <thato.pabane@bmwithub.co.za>
2025-06-07 00:07:42 +0200

    Add Service To Call Backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
@@ -16,7 +17,8 @@ import { AddUserComponent } from './components/add-user/add-user.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../app.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private host = 'http://localhost:8000/';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.host + 'users.php');
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(this.host + 'register.php', user);
+  }
+}
