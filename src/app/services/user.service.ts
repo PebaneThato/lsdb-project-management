0dcc512d4f3dba8f63a52f09b5de6531902372ff
Thato Pebane <jpebane@gmail.com>
2025-06-07 04:02:13 +0200

    Add Support to CORS requests

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,15 +7,15 @@ import { User } from '../app.interface';
   providedIn: 'root'
 })
 export class UserService {
-  private host = 'http://localhost:8000/';
+  // private host = 'http://localhost:8000/';
 
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.host + 'users.php');
+    return this.http.get<User[]>('/api/users.php');
   }
 
   createUser(user: User): Observable<User> {
-    return this.http.post<User>(this.host + 'register.php', user);
+    return this.http.post<User>('/api/register.php', user);
   }
 }
